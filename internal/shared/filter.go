7f08259c0ded8a7754ae37d292a1ee7e18fec094
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2025-02-24 01:27:06 +0300

    Update filter documentation comments.

--- internal/shared/filter.go
+++ internal/shared/filter.go
@@ -9,9 +9,11 @@ package shared
 //   - input: A slice of elements of type T to be filtered.
 //   - predicate: A function that takes an element of type T and returns a boolean.
 //                The element is included in the result if the predicate returns true.
+//  - selector: A function that takes an element of type T and returns a result of type TResult.
+// 			    It is used to transform each element of the input slice that satisfies the predicate.
 //
 // Returns:
-//   A slice of elements of type T that satisfy the predicate function.
+//   A slice of elements of type TResult that satisfy the predicate function.
 func Filter[T any, TResult any](input []T, predicate func(T) bool, selector func(T) TResult) (result []TResult) {
 	for _, value := range input {
 		if predicate(value) {
