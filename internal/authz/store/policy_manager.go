96fb4d23874c5fe273257c643fc7ddcbc03a13d5
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2025-03-29 22:34:01 +0300

    Add context.Context parameter to PolicyManager methods for improved context handling

--- internal/authz/store/policy_manager.go
+++ internal/authz/store/policy_manager.go
@@ -1,15 +1,19 @@
 package store
 
-import "github.com/salmarsumi/recipes/internal/authz"
+import (
+	"context"
+
+	"github.com/salmarsumi/recipes/internal/authz"
+)
 
 // PolicyManager defines the operations needed to manage the policy store.
 type PolicyManager[TGroupId any, TPermissionId any, TUserId any] interface {
-	UpdateGroupPermissions(groupId TGroupId, permissions []TPermissionId) error
-	UpdateGroupUsers(groupId TGroupId, users []TUserId) error
-	UpdateUserGroups(userId TUserId, groups []TGroupId) error
-	CreateGroup(groupId TGroupId, groupName string) error
-	DeleteGroup(groupId TGroupId) error
-	ChangeGroupName(groupId TGroupId, newGroupName string) error
-	DeleteUser(userId TUserId) error
-	ReadPolicy() (*authz.Policy, error)
+	UpdateGroupPermissions(ctx context.Context, groupId TGroupId, permissions []TPermissionId) error
+	UpdateGroupUsers(ctx context.Context, groupId TGroupId, users []TUserId) error
+	UpdateUserGroups(ctx context.Context, userId TUserId, groups []TGroupId) error
+	CreateGroup(ctx context.Context, groupId TGroupId, groupName string) error
+	DeleteGroup(ctx context.Context, groupId TGroupId) error
+	ChangeGroupName(ctx context.Context, groupId TGroupId, newGroupName string) error
+	DeleteUser(ctx context.Context, userId TUserId) error
+	ReadPolicy(ctx context.Context) (*authz.Policy, error)
 }
